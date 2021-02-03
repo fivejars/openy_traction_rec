8a387ee180990fc24fd69610c83efae08cee4db3
Vasyl Manachynskyi <v.manachynskyi@gmail.com>
2021-02-03 12:48:25 +0200

    [YP2-5] merge user-authorizer method in custom module.

---
+++ src/YpkcUserAuthorizer.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\ypkc_salesforce;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
+/**
+ * User Authorizer class.
+ */
+class YpkcUserAuthorizer {
+
+  /**
+   * User entity storage.
+   *
+   * @var \Drupal\User\UserStorageInterface
+   */
+  protected $userStorage;
+
+  /**
+   * YpkcUserAuthorizer constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   Entity Type Manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->userStorage = $entityTypeManager->getStorage('user');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function authorizeUser($name, $email, array $extra_data = []) {
+
+    if (empty($name) || empty($email)) {
+      return;
+    }
+
+    // Create drupal user if it doesn't exist and login it.
+    $account = user_load_by_mail($email);
+    if (!$account) {
+      $user = $this->userStorage->create();
+      $user->setPassword(user_password());
+      $user->enforceIsNew();
+      $user->setEmail($email);
+      $user->setUsername($name);
+      $user->activate();
+      $result = $account = $user->save();
+      if ($result) {
+        $account = user_load_by_mail($email);
+      }
+    }
+    else {
+      // Activate user if it's not.
+      if (!$account->isActive()) {
+        $account->activate();
+        $account->setPassword(user_password());
+        $account->save();
+      }
+    }
+
+    user_login_finalize($account);
+
+  }
+
+}
