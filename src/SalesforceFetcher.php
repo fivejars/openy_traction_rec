3b9a3790952aaca4e1f0031b12ad6471dac26a87
Dmitry <dmitry.chubar@fivejars.com>
2021-04-12 22:38:28 +0300

    [YPKCOY-190]: Don't fetch classes with closed online registration

--- src/SalesforceFetcher.php
+++ src/SalesforceFetcher.php
@@ -107,6 +107,7 @@ class SalesforceFetcher {
     FROM TREX1__Course_Session_Option__c 
     WHERE TREX1__Course_Option__r.TREX1__Available_Online__c = true 
       AND TREX1__Course_Option__r.TREX1__Day_of_Week__c  != null 
+      AND TREX1__Course_Option__r.TREX1__Register_Online_To_Date__c > YESTERDAY
       AND TREX1__Course_Option__r.TREX1__Start_Date__c != null');
 
     $result = $this->simplify($result);
