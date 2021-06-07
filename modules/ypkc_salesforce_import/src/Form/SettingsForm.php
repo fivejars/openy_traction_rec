9a8a5728a083fd10998b714a819ab1766881e595
Dmitry <dmitry.chubar@fivejars.com>
2021-06-07 22:28:17 +0300

    [YP2-XX]: Clean up old imported JSON files

--- modules/ypkc_salesforce_import/src/Form/SettingsForm.php
+++ modules/ypkc_salesforce_import/src/Form/SettingsForm.php
@@ -37,6 +37,36 @@ class SettingsForm extends ConfigFormBase {
       '#description' => $this->t('Enable Salesforce import'),
     ];
 
+    $form['backup_json'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Backup'),
+      '#default_value' => $config->get('backup_json'),
+      '#description' => $this->t('Save imported JSON files'),
+    ];
+
+    $form['backup_limit'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Backup limit'),
+      '#default_value' => $config->get('backup_limit'),
+      '#options' => [
+        5 => 5,
+        10 => 10,
+        15 => 15,
+        20 => 20,
+        25 => 25,
+        35 => 25,
+        40 => 40,
+        45 => 45,
+        50 => 50,
+      ],
+      '#description' => $this->t('The max number of folders with imported JSON files to store'),
+      '#states' => [
+        'visible' => [
+          ':input[name="backup_json"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -46,8 +76,10 @@ class SettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('ypkc_salesforce_import.settings');
     $values = $form_state->getValues();
-    $config->set('enabled', $values['enabled'])
-      ->save();
+    $config->set('enabled', $values['enabled']);
+    $config->set('backup_json', $values['backup_json']);
+    $config->set('backup_limit', $values['backup_limit']);
+    $config->save();
 
     parent::submitForm($form, $form_state);
   }
