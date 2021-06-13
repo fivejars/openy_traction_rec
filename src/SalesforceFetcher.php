50625578becd3148d84df9937cc36e06b21c150a
Dmitry <dmitry.chubar@fivejars.com>
2021-06-13 19:51:20 +0300

    [YPKC3-8]: TractionRec services refactoring

--- src/SalesforceFetcher.php
+++ src/SalesforceFetcher.php
@@ -3,6 +3,7 @@
 namespace Drupal\ypkc_salesforce;
 
 use Drupal\Core\File\FileSystemInterface;
+use TractionRecInterface;
 
 /**
  * Contains related to fetching from Salesforce functionality.
@@ -17,11 +18,11 @@ class SalesforceFetcher {
   protected $storagePath = 'private://salesforce_import/json/';
 
   /**
-   * Traction Rec Client service.
+   * Traction Rec wrapper.
    *
-   * @var \Drupal\ypkc_salesforce\TractionRecClient
+   * @var \TractionRecInterface
    */
-  protected $tractionRecClient;
+  protected $tractionRec;
 
   /**
    * The file system service.
@@ -40,13 +41,14 @@ class SalesforceFetcher {
   /**
    * SalesforceFetcher Constructor.
    *
-   * @param \Drupal\ypkc_salesforce\TractionRecClient $traction_rec_client
-   *   The Traction Rec client service.
+   * @param \TractionRecInterface $traction_rec
+   *   The Traction Rec wrapper.
+   *
    * @param \Drupal\Core\File\FileSystemInterface $file_system
    *   The file handler.
    */
-  public function __construct(TractionRecClient $traction_rec_client, FileSystemInterface $file_system) {
-    $this->tractionRecClient = $traction_rec_client;
+  public function __construct(TractionRecInterface $traction_rec, FileSystemInterface $file_system) {
+    $this->tractionRec = $traction_rec;
     $this->fileSystem = $file_system;
 
     $this->fileSystem->prepareDirectory($this->storagePath, FileSystemInterface::CREATE_DIRECTORY);
@@ -72,45 +74,7 @@ class SalesforceFetcher {
    * @throws \GuzzleHttp\Exception\GuzzleException
    */
   public function fetchSessions() {
-    $result = $this->tractionRecClient->executeQuery('SELECT
-      TREX1__Course_Option__r.id,
-      TREX1__Course_Option__r.name,
-      TREX1__Course_Option__r.TREX1__Available_Online__c,
-      TREX1__Course_Option__r.TREX1__Available__c,
-      TREX1__Course_Option__r.TREX1__capacity__c,
-      TREX1__Course_Option__r.TREX1__Start_Date__c,
-      TREX1__Course_Option__r.TREX1__Start_Time__c,
-      TREX1__Course_Option__r.TREX1__End_Date__c,
-      TREX1__Course_Option__r.TREX1__End_Time__c,
-      TREX1__Course_Option__r.TREX1__Day_of_Week__c,
-      TREX1__Course_Option__r.TREX1__Instructor__c,
-      TREX1__Course_Option__r.TREX1__Location__c,
-      TREX1__Course_Option__r.TREX1__Location__r.id,
-      TREX1__Course_Option__r.TREX1__Location__r.name,
-      TREX1__Course_Option__r.TREX1__Age_Max__c,
-      TREX1__Course_Option__r.TREX1__Age_Min__c,
-      TREX1__Course_Option__r.TREX1__Register_Online_From_Date__c,
-      TREX1__Course_Option__r.TREX1__Register_Online_From_Time__c,
-      TREX1__Course_Option__r.TREX1__Register_Online_To_Date__c,
-      TREX1__Course_Option__r.TREX1__Register_Online_To_Time__c,
-      TREX1__Course_Option__r.TREX1__Registration_Total__c,
-      TREX1__Course_Option__r.TREX1__Total_Capacity_Available__c,
-      TREX1__Course_Option__r.TREX1__Type__c,
-      TREX1__Course_Option__r.TREX1__Unlimited_Capacity__c,
-      TREX1__Course_Session__r.TREX1__Course__r.name,
-      TREX1__Course_Session__r.TREX1__Course__r.id,
-      TREX1__Course_Session__r.TREX1__Course__r.TREX1__Description__c,
-      TREX1__Course_Option__r.TREX1__Product__c,
-      TREX1__Course_Option__r.TREX1__Product__r.id,
-      TREX1__Course_Option__r.TREX1__Product__r.name,
-      TREX1__Course_Option__r.TREX1__Product__r.TREX1__Price_Description__c
-    FROM TREX1__Course_Session_Option__c 
-    WHERE TREX1__Course_Option__r.TREX1__Available_Online__c = true 
-      AND TREX1__Course_Option__r.TREX1__Day_of_Week__c  != null 
-      AND TREX1__Course_Option__r.TREX1__Register_Online_To_Date__c > YESTERDAY
-      AND TREX1__Course_Option__r.TREX1__Start_Date__c != null');
-
-    $result = $this->simplify($result);
+    $result = $this->tractionRec->loadCourseOptions();
 
     if (empty($result['records'])) {
       return [];
@@ -141,9 +105,8 @@ class SalesforceFetcher {
    * @throws \Drupal\ypkc_salesforce\InvalidTokenException
    * @throws \GuzzleHttp\Exception\GuzzleException
    */
-  protected function paginationFetch(string $nextUrl, JsonStreamDumper $dumper) {
-    $result = $this->tractionRecClient->send('GET', 'https://ymcapkc.my.salesforce.com' . $nextUrl);
-    $result = $this->simplify($result);
+  protected function paginationFetch(string $nextUrl, JsonStreamDumper $dumper): array {
+    $result = $this->tractionRec->loadNextPage($nextUrl);
 
     if (empty($result['records'])) {
       return [];
@@ -164,22 +127,9 @@ class SalesforceFetcher {
    *
    * @return array
    *   The array of fetched locations.
-   *
-   * @throws \Drupal\ypkc_salesforce\InvalidTokenException
-   * @throws \GuzzleHttp\Exception\GuzzleException
    */
   public function fetchLocations(): array {
-    $result = $this->tractionRecClient->executeQuery('SELECT
-      TREX1__Location__c.id,
-      TREX1__Location__c.name,
-      TREX1__Location__c.TREX1__Address_City__c,
-      TREX1__Location__c.TREX1__Address_Country__c,
-      TREX1__Location__c.TREX1__Address_State__c,
-      TREX1__Location__c.TREX1__Address_Street__c,
-      TREX1__Location__c.TREX1__Address_Postal_Code__c
-    FROM TREX1__Location__c');
-
-    $result = $this->simplify($result);
+    $result = $this->tractionRec->loadLocations();
 
     if (empty($result['records'])) {
       return [];
@@ -196,22 +146,9 @@ class SalesforceFetcher {
 
   /**
    * Fetches classes.
-   *
-   * @throws \Drupal\ypkc_salesforce\InvalidTokenException
-   * @throws \GuzzleHttp\Exception\GuzzleException
    */
   public function fetchClasses():void {
-    $result = $this->tractionRecClient->executeQuery('SELECT
-      TREX1__Course__c.id,
-      TREX1__Course__c.name,
-      TREX1__Course__c.TREX1__Description__c,
-      TREX1__Course__c.TREX1__Rich_Description__c,
-      TREX1__Course__c.TREX1__Program__r.id,
-      TREX1__Course__c.TREX1__Program__r.name,
-      TREX1__Course__c.TREX1__Available__c
-    FROM TREX1__Course__c WHERE TREX1__Available_Online__c = true');
-
-    $result = $this->simplify($result);
+    $result = $this->tractionRec->loadCourses();
 
     if (empty($result['records'])) {
       return;
@@ -222,25 +159,9 @@ class SalesforceFetcher {
 
   /**
    * Fetches the program data.
-   *
-   * @throws \Drupal\ypkc_salesforce\InvalidTokenException
-   * @throws \GuzzleHttp\Exception\GuzzleException
    */
   public function fetchProgramAndCategories():void {
-    $result = $this->tractionRecClient->executeQuery(
-      'SELECT
-      TREX1__Program_Category_Tag__c.id,
-      TREX1__Program_Category_Tag__c.name,
-      TREX1__Program_Category_Tag__c.TREX1__Program__r.id,
-      TREX1__Program_Category_Tag__c.TREX1__Program__r.name,
-      TREX1__Program_Category_Tag__c.TREX1__Program__r.TREX1__Available__c,
-      TREX1__Program_Category_Tag__c.TREX1__Program_Category__r.id,
-      TREX1__Program_Category_Tag__c.TREX1__Program_Category__r.name,
-      TREX1__Program_Category_Tag__c.TREX1__Program_Category__r.TREX1__Available__c
-    FROM TREX1__Program_Category_Tag__c WHERE TREX1__Program__r.TREX1__Available_Online__c = true AND TREX1__Program_Category__r.TREX1__Available_Online__c = true'
-    );
-
-    $result = $this->simplify($result);
+    $result = $this->tractionRec->loadProgramCategoryTags();
 
     if (empty($result['records'])) {
       return;
@@ -277,32 +198,6 @@ class SalesforceFetcher {
     $file = fopen($filename, 'w');
     fwrite($file, json_encode($data, JSON_PRETTY_PRINT | JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT));
     fclose($file);
-  }
-
-  /**
-   * Clean up TractionRec extra prefixes and suffixes for easier usage.
-   *
-   * @param array $array
-   *   Response result from Salesfoce.
-   *
-   * @return array
-   *   Results with cleaned keys.
-   */
-  private function simplify(array $array): array {
-    $new_array = [];
-    foreach ($array as $key => $value) {
-      $new_key = str_replace(['TREX1__', '__c', '__r'], '', $key);
-      if ($new_key === 'attributes') {
-        continue;
-      }
-      if (is_array($value)) {
-        $new_array[$new_key] = $this->simplify($value);
-      }
-      else {
-        $new_array[$new_key] = $value;
-      }
-    }
-    return $new_array;
   }
 
   /**
