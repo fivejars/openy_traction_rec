6ef731026bd710c8e3c1216543376d650a02c799
Dmitry <dmitry.chubar@fivejars.com>
2021-07-06 19:05:40 +0300

    [YPKC3-6]: Remove paragraph cleaner. 'entity_reference_revisions_orphan_purger' does the job.

--- modules/ypkc_salesforce_import/src/Cleaner.php
+++ modules/ypkc_salesforce_import/src/Cleaner.php
@@ -71,13 +71,6 @@ class Cleaner {
   }
 
   /**
-   * Starts database clean up.
-   */
-  public function cleanDatabase($limit = 5000) {
-    $this->deleteOldTimeParagraphs($limit);
-  }
-
-  /**
    * Cleans JSON files from backup folder.
    */
   public function cleanBackupFiles() {
@@ -104,39 +97,6 @@ class Cleaner {
       if (!$process->isSuccessful()) {
         $this->logger->warning('Impossible to remove JSON files: ' . $process->getErrorOutput());
       }
-    }
-  }
-
-  /**
-   * Removes session_time paragraphs not associated with nodes.
-   */
-  protected function deleteOldTimeParagraphs($limit = 5000) {
-    try {
-      $count = 0;
-
-      $query = $this->database->select('paragraphs_item_field_data', 'pifd');
-      $query->addField('pifd', 'id');
-      $query->condition('pifd.type', 'session_time');
-      $query->leftJoin('node__field_session_time', 'nfst', 'nfst.field_session_time_target_id = pifd.id');
-      $query->isNull('nfst.field_session_time_target_id');
-      $query->range(0, $limit);
-      $result = $query->execute()->fetchCol();
-
-      if (empty($result)) {
-        return FALSE;
-      }
-
-      $storage = $this->entityTypeManager->getStorage('paragraph');
-      foreach (array_chunk($result, 50) as $chunk) {
-        $storage->delete($storage->loadMultiple($chunk));
-        $count += 50;
-      }
-
-      return $count;
-    }
-    catch (\Exception $e) {
-      $this->logger->error('SF Clean up error: ' . $e->getMessage());
-      return $count;
     }
   }
 
